c47ac85a8cde364e5dd69aba61055aba810eba1b
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2025-05-28 16:33:21 -0400

    Create MainPage.xaml.cs

---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Time_Managmeent_System
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
