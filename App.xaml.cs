ef117c0695d2a10c41c8e1cbc45628ae63179b48
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2025-05-28 16:59:49 -0400

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@
         public App()
         {
             InitializeComponent();
+            MainPage = new NavigationPage(new Pages.LoginPage());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
