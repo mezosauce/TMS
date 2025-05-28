39aa1142541b85e1766d4d619d24c0f204c68681
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2025-05-28 18:16:44 -0400

    Update Login.xaml.cs

--- Pages/Login.xaml.cs
+++ Pages/Login.xaml.cs
@@ -12,19 +12,19 @@ namespace Time_Managmeent_System.Pages
         private async void OnEmployeeLoginClicked(object sender, EventArgs e)
         {
             // Navigate to Employee login or main page
-            await Navigation.PushAsync(new MainPage());
+            await Navigation.PushAsync(new LoginType.Employee());
         }
 
         private async void OnManagerLoginClicked(object sender, EventArgs e)
         {
             // Navigate to Manager login or main page
-            await Navigation.PushAsync(new MainPage());
+            await Navigation.PushAsync(new LoginType.Manager());
         }
 
         private async void OnAdminLoginClicked(object sender, EventArgs e)
         {
             // Navigate to Admin login or main page
-            await Navigation.PushAsync(new MainPage());
+            await Navigation.PushAsync(new LoginType.Admin());
         }
     }
 }
