8ce3853a390faab1c1e421ba016934e51569cf05
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2025-05-28 17:33:34 -0400

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,13 +5,13 @@
         public App()
         {
             InitializeComponent();
-            // Set the main page to the login page
-            MainPage = new NavigationPage(new Pages.LoginPage());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            // Set the root page of the application in the CreateWindow method
+            var window = new Window(new NavigationPage(new Pages.LoginPage()));
+            return window;
         }
     }
 }
