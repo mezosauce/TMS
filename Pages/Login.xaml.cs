2d7da23f56cbd9dd2523c41708e64f2b7d9f44f2
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2025-05-29 12:16:21 -0400

    Update Login.xaml.cs

--- Pages/Login.xaml.cs
+++ Pages/Login.xaml.cs
@@ -26,9 +26,10 @@ namespace Time_Managmeent_System.Pages
             // Navigate to Admin login or main page
             await Navigation.PushAsync(new LoginType.Admin());
         }
-        private async void OnGuestLoginClicked(object sender, EventArgs e)
+        private async void OnRegisterClicked(object sender, EventArgs e)
         {
             // Navigate to Guest login or main page
             await Navigation.PushAsync(new LoginType.Guest());
         }
+    }
 }
