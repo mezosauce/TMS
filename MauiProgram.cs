b1f75480219cc133635376c4982e15d2bf3de4b6
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2025-05-29 13:50:03 -0400

    Update MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -8,15 +8,33 @@ namespace Time_Managmeent_System
         {
             var builder = MauiApp.CreateBuilder();
             builder
+
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
+#if WINDOWS
+                fonts.AddFont("fa-solid-900.otf", "FontAwesome");
+                fonts.AddFont("fa-brands-400.otf", "FontAwesomeBrands");
+                fonts.AddFont("fa-regular-400.otf", "FontAwesomeRegular");
+#elif MACCATALYST
+                fonts.AddFont("fa-solid-900.otf", "FontAwesome");
+                fonts.AddFont("fa-brands-400.otf", "FontAwesomeBrands");
+                fonts.AddFont("fa-regular-400.otf", "FontAwesomeRegular");
+#elif IOS
+                fonts.AddFont("fa-solid-900.otf", "FontAwesome");
+                fonts.AddFont("fa-brands-400.otf", "FontAwesomeBrands");
+                fonts.AddFont("fa-regular-400.otf", "FontAwesomeRegular");
+#elif ANDROID
+                fonts.AddFont("fa-solid-900.otf", "FontAwesome");
+                fonts.AddFont("fa-brands-400.otf", "FontAwesomeBrands");
+                fonts.AddFont("fa-regular-400.otf", "FontAwesomeRegular");
+#endif
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
