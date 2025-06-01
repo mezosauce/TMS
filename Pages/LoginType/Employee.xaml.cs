3e063bcc46c2dc3971e56ccbd9413e67923a99f0
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2025-06-01 14:32:26 -0400

    Update Employee.xaml.cs

--- Pages/LoginType/Employee.xaml.cs
+++ Pages/LoginType/Employee.xaml.cs
@@ -12,6 +12,6 @@ public partial class Employee : ContentPage
         // Implement your login logic here
         // For example, validate credentials and navigate to the admin dashboard
         await DisplayAlert("Login", "Employee login successful!", "OK");
-        await Navigation.PushAsync(new MainPage()); // Navigate to the main page after login
+        await Navigation.PushAsync(new Dashboard.EmployeeDash()); // Navigate to the main page after login
     }
 }
