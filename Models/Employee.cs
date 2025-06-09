9e02670128a536f7cd48faaead713497ac25043f
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2025-06-09 11:06:33 -0400

    asd

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -15,14 +15,19 @@ public class Employee : BaseModel
 
     [PrimaryKey("id", false)]
     public int Id { get; set; }
+
     [Column("Username")]
     public string Username { get; set; }
+
     [Column("Password")]
     public string Password { get; set; }
+
     [Column("Position")]
     public string Position { get; set; }
+
     [Column("First")]
     public string First { get; set; }
+
     [Column("Last")]
     public string Last { get; set; }
 }
